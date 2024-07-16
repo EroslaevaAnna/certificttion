33b7b6179fc7071a8f5f5526b30d70bfe62e8570
yhy <eroslaevaanna@gmail.com>
2024-07-16 22:52:19 +0400

    redact

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
-public class Task1 {    private static final int letterCount =15;
-    private static final int spaceCount =3;    private static final int letter3 =62;
+
+public class Task1 {
+    private static final int letterCount = 15;
+    private static final int spaceCount = 3;
+    private static final int letter3 = 62;
     private static final int space = 12;
-    public static void main(String[] args) {        Scanner myScanner = new Scanner(System.in);
-        int fenceLength;        System.out.print("Введите длинну забора ");
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        int fenceLength;
+        System.out.print("Введите длинну забора ");
         fenceLength = Integer.parseInt(myScanner.nextLine());
-        int stringLength = letter3 * (letterCount / 3) + spaceCount * space;        if (fenceLength < stringLength)
-            System.out.println("Надпись не поместится!");        else
+        int stringLength = letter3 * (letterCount / 3) + spaceCount * space;
+        if (fenceLength < stringLength)
+            System.out.println("Надпись не поместится!");
+        else
             System.out.println("Надпись поместится! Можно признаться в любви!");
     }
 }
--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -1,6 +1,10 @@
-import java.util.Arrays;import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task3 {
-    public static void main(String[] args) {        int [] arr = {9, 2, 3, 1, 8, 6, 12};
+    public static void main(String[] args) {
+        int[] arr = {9, 2, 3, 1, 8, 6, 12};
         Arrays.sort(arr);        // сортируем массив
-        System.out.println("Второй по величине элемент: " + arr[arr.length-2]);    }
+        System.out.println("Второй по величине элемент: " + arr[arr.length - 2]);
+    }
 }
